b9819023ac08a30c58ddd6e465e71ab21fb3adf0
Kamil Doległo <kamilok1965@interia.pl>
2019-03-22 11:58:29 +0100

    Rework configuration, fix Gradle plugin

--- core/src/main/kotlin/Java/JavadocParser.kt
+++ core/src/main/kotlin/Java/JavadocParser.kt
@@ -229,9 +229,8 @@ class JavadocParser(
             linkSignature != null -> {
                 val linkNode =
                         ContentNodeLazyLink(
-                                (tag.valueElement ?: linkElement).text,
-                                { -> refGraph.lookupOrWarn(linkSignature, logger) }
-                        )
+                                (tag.valueElement ?: linkElement).text
+                        ) { -> refGraph.lookupOrWarn(linkSignature!!, logger) }
                 linkNode.append(text)
                 linkNode
             }
